bc62297f67718ffec5c28c93b10010f2ee1f52eb
Skylark355 <adzesi.chris@gmail.com>
2025-01-04 21:04:20 +0000

    res

--- JS/script.js
+++ JS/script.js
@@ -35,3 +35,23 @@ var swiper = new Swiper(".contents", {
   },
 });
 // swipper end
+
+// hamburger code starts here
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const hideSidebar = document.querySelector(".sidebar");
+  hideSidebar.style.display = "none";
+}
+
+const navItems = document.querySelectorAll(".navbar-item"); // Select all navbar items
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    hideSidebar(); // Hide the sidebar on click
+  });
+});
+
+// hamburger codes ends here
